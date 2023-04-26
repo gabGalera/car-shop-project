92a4a5bebfff1282d96cf579ab4b23ac24adf68b
Gabriel Galera <gabgalera@hotmail.com>
2023-04-26 11:23:32 -0300

    Feat: Car.ts

---
+++ src/Domains/Car.ts
@@ -0,0 +1,34 @@
+import ICar from '../Interfaces/ICar';
+
+class Car {
+  private id: string | undefined;
+  private model: string;
+  private year: number;
+  private color: string;
+  private status: boolean;
+  private buyValue: number;
+  private doorsQty: number;
+  private seatsQty: number;
+
+  constructor(
+    { 
+      model, 
+      year, 
+      color, 
+      status, 
+      buyValue,
+      doorsQty,
+      seatsQty, 
+    }: ICar,
+  ) {
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.status = status || false;
+    this.buyValue = buyValue;
+    this.doorsQty = doorsQty;
+    this.seatsQty = seatsQty;
+  }
+}
+
+export default Car;
